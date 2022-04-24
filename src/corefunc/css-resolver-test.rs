c6d4b8769fd5b28a2b5cd15738ff39320305c953
feame <71621797+vicimeans@users.noreply.github.com>
2022-04-24 23:26:50 +0800

    css-to-renderlist

--- src/corefunc/css-resolver-test.rs
+++ src/corefunc/css-resolver-test.rs
@@ -1,5 +1,5 @@
-
-use super::css_resolver::*;
+use super::css_resolver::{extract_css_basic_class, is_css_file, walk_css_dir,CssRenderList};
+use std::path::Path;
 #[test]
 fn path_name_test() {
     use std::ffi::OsStr;
@@ -8,9 +8,56 @@ fn path_name_test() {
     assert_eq!(pathbuf.iter().last(), Some(OsStr::new("css")));
 }
 #[test]
-fn css_name_test() {
+fn is_css_file_test() {
     use std::path::Path;
 
     let css_tar = Path::new("t.css");
     assert!(is_css_file(css_tar));
+}
+#[test]
+#[ignore]
+fn walk_css_dir_test() {
+    let res = walk_css_dir(Path::new(r"css")).unwrap();
+    assert_eq!(res.map(|v| v.len()), Some(5));
+}
+#[test]
+#[ignore]
+fn extract_css_basic_class_test() {
+    let test1 = Path::new(r"test-material\test1.css");
+     let test2=Path::new(r"test-material\test2.css");
+     let test3=Path::new(r"test-material\test3.css");
+    let res1 = extract_css_basic_class(test1);
+    let res2=extract_css_basic_class(test2);
+     let res3=extract_css_basic_class(test3);
+     println!("{:?}",res1);
+     println!("{:?}",res2);
+     println!("{:?}",res3);
+    assert_eq!(
+        res1,
+        Some(vec![
+            "intro".to_owned(),
+            "intro1".to_owned(),
+            "intro2".to_owned(),
+            "intro3".to_owned(),
+            "intro4".to_owned()
+        ])
+    );
+      assert_eq!(res2,Some(vec!["kkk".to_owned()]));
+     assert_eq!(res3,None);
+}
+use regex::Regex;
+#[test]
+fn css_class_regex_test() {
+    let tar = r".kkk-button {";
+
+    let pattern_res = Regex::new(r"\..+\-([0-9[:alpha:]\-_]+)\s*\{").unwrap();
+    assert_eq!(
+        pattern_res.captures(tar).unwrap().get(1).unwrap().as_str(),
+        "button"
+    );
+}
+#[test]
+fn renderlist_test() {
+    let res=CssRenderList::new(Path::new(r"D:\LAB\Rust\Project\beaver-code-repairer\css"));
+    println!("{:#?}",res);
 }
--- src/corefunc/css_resolver.rs
+++ src/corefunc/css_resolver.rs
@@ -1,14 +1,17 @@
+use regex::Regex;
+use std::collections::HashMap;
 use std::ffi::OsStr;
 use std::fs;
 use std::io;
+use std::io::BufRead;
 use std::path::Path;
 use std::path::PathBuf;
 pub fn is_css_file<P: AsRef<Path>>(path: P) -> bool {
     let suffix = path.as_ref().extension();
-    let suffix_str= suffix.map(|s|s.to_str());
+    let suffix_str = suffix.map(|s| s.to_str());
     suffix_str == Some(Some("css"))
 }
-pub fn walk_css_dir<P: AsRef<Path>>(folder: &P) -> io::Result<Option<Vec<Box<PathBuf>>>> {
+pub fn walk_css_dir<P: AsRef<Path>>(folder: P) -> io::Result<Option<Vec<Box<PathBuf>>>> {
     let path = folder.as_ref();
     let pathbuf = path.to_path_buf();
     let mut res_vec = vec![];
@@ -17,7 +20,7 @@ pub fn walk_css_dir<P: AsRef<Path>>(folder: &P) -> io::Result<Option<Vec<Box<Pat
             if let Ok(entrys) = fs::read_dir(path) {
                 for entry in entrys {
                     let css_file = entry?.path();
-                    let css_file_out=css_file.clone();
+                    let css_file_out = css_file.clone();
                     if is_css_file(css_file) {
                         res_vec.push(Box::new(css_file_out));
                     }
@@ -31,5 +34,93 @@ pub fn walk_css_dir<P: AsRef<Path>>(folder: &P) -> io::Result<Option<Vec<Box<Pat
         }
     } else {
         Ok(None)
+    }
+}
+pub fn extract_css_basic_class<P: AsRef<Path>>(path: P) -> Option<Vec<String>> {
+    let mut classes_res = fs::read_to_string(path);
+    if let Ok(classes) = classes_res {
+        let pattern_res = Regex::new(r"\..+\-([0-9[:alpha:]\-_]+)\s*\{");
+        if let Ok(pattern) = pattern_res {
+            let mut cursor = io::Cursor::new(classes);
+            let mut buffer = String::new();
+            let mut res = vec![];
+            while let Ok(num) = cursor.read_line(&mut buffer) {
+                if num == 0 {
+                    break;
+                }
+                let class_name_opt = pattern.captures(&buffer);
+                if let Some(class_name) = class_name_opt {
+                    res.push(class_name[1].to_string());
+                }
+                buffer.clear()
+            }
+            Some(res)
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
+fn cross_vec_str(
+    main_key: &Vec<String>,
+    sub_key: &Vec<String>,
+) -> HashMap<String, HashMap<String, String>> {
+    let mut res = HashMap::new();
+    for main in main_key.iter() {
+        let mut subs = HashMap::new();
+        for sub in sub_key.iter() {
+            subs.insert(main.clone(), main.to_owned() + "-" + sub);
+        }
+        res.insert(main.clone(), subs);
+    }
+    res
+}
+
+#[derive(Debug, Default)]
+pub struct CssRenderList {
+    themelist: Vec<String>,
+    widgitlist: Vec<String>,
+    renderlist: HashMap<String, HashMap<String, String>>,
+}
+impl CssRenderList {
+  pub(crate)  fn new<P: AsRef<Path>>(path: P) -> Self {
+        if let Ok(Some(vec_csses)) = walk_css_dir(path) {
+            if let Some(first_css) = vec_csses.first() {
+                let mut widgitlist = vec![];
+                let mut themelist = vec![];
+                let widgets: Vec<String> = match extract_css_basic_class(first_css.as_ref()) {
+                    Some(w) => w,
+                    _ => return Default::default(),
+                };
+                widgets.into_iter().for_each(|s| widgitlist.push(s));
+
+                for css in vec_csses.iter() {
+                    let os_str = css.file_name().map(|s| match PathBuf::from(s).file_stem() {
+                        Some(s) => s.to_os_string(),
+                        _ => return Default::default(),
+                    });
+                    let theme_opt = os_str.map(|s| match s.to_str() {
+                        Some(s) => s.to_string(),
+                        _ => return Default::default(),
+                    });
+                    if let Some(theme) = theme_opt {
+                        themelist.push(theme);
+                    } else {
+                        return Default::default();
+                    }
+                }
+                let renderlist = cross_vec_str(&themelist, &widgitlist);
+                Self {
+                    themelist: themelist,
+                    widgitlist: widgitlist,
+                    renderlist: renderlist,
+                }
+            } else {
+                Default::default()
+            }
+        } else {
+            Default::default()
+        }
     }
 }
